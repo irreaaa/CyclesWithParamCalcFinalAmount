42e276eefbadcc69a901e21e051e623f3973e04e
Irina ྀིྀིྀིྀིྀིྀིྀ <146268256+irreaaa@users.noreply.github.com>
2024-10-24 00:07:53 +0300

    sec commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите значение нижнего предела интегрирования: ");
+﻿//StreamWriter txt = new StreamWriter("C:/Users/prdb/source/cbufb/ConsoleAppIra6/test.txt");
+Console.WriteLine("Введите значение нижнего предела интегрирования: ");
 double a = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("Введите значение верхнего предела интегрирования: ");
@@ -11,21 +12,23 @@ if (a >= b)
 }
 
 Console.WriteLine("Введите количество разбиений интеграла на треугольники: ");
-double n = Convert.ToDouble(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
 
 
 double h = (b - a) / n;
-
+    
 Func<double, double> f = x => 2 * Math.Pow(x, 2) + 3 * x;
 
 double integral = 0;
-for (int i = 0; i < n; i++)
+for (int i = 1; i <= n; i++)
 {
-    double xMiddle = a + (i + 0.5) * h;
+    double xMiddle = a + (i - 1 + 0.5) * h;
 
     integral += f(xMiddle) * h;
 }
 
-Console.WriteLine($"Приближённое значение интеграла методом средних треугольников: {integral}");
+Console.WriteLine($"Приближённое значение интеграла методом средних треугольников: {integral:F4}");
+//txt.WriteLine($"Приближённое значение интеграла методом средних треугольников: {integral}");
+//txt.Close();
 
 Console.ReadKey();
